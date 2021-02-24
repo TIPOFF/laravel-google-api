f7f03a8c8b09ea13e82bdf917c74054402b30e6a
Gustavo Gentil <gustavorgentil@outlook.com>
2021-02-24 15:04:14 -0300

    Rename test class

---
+++ tests/Unit/Models/KeyTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tipoff\GoogleApi\Tests\Unit\Models;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tipoff\GoogleApi\Models\Key;
+use Tipoff\GoogleApi\Tests\TestCase;
+
+class KeyTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    /** @test */
+    public function it_has_a_slug()
+    {
+        $slug = $this->faker->slug;
+        $key = Key::factory()->create(['slug' => $slug]);
+
+        $this->assertEquals($slug, $key->slug);
+    }
+
+    /** @test */
+    public function it_has_a_value()
+    {
+        $value = $this->faker->word;
+        $key = Key::factory()->create(['value' => $value]);
+
+        $this->assertEquals($value, $key->value);
+    }
+}
