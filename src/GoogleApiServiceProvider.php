b41ea8822ed658b60db28ee1dd8cde4d5184eb19
Kyle Barney <kyle@kylebarney.dev>
2021-02-27 12:46:55 -0700

    Add YouTube service binding and abstract out client configuration

--- src/GoogleApiServiceProvider.php
+++ src/GoogleApiServiceProvider.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Tipoff\GoogleApi;
 
+use Exception;
 use Google_Client;
 use Google_Service_MyBusiness;
+use Google_Service_YouTube;
 use Tipoff\GoogleApi\Models\Key;
 use Tipoff\Support\TipoffPackage;
 use Tipoff\Support\TipoffServiceProvider;
@@ -29,26 +31,40 @@ class GoogleApiServiceProvider extends TipoffServiceProvider
         });
 
         $this->app->bind(Google_Service_MyBusiness::class, function () {
-            $client = app()->make(Google_Client::class);
-
-            $client->setAuthConfig(config('google-api.my-business.client-secret'));
-            $client->addScope(['https://www.googleapis.com/auth/business.manage']);
-            $client->setAccessType('offline');
-
-            $token = json_decode(Key::where('slug', config('google-api.my-business.access-token-slug'))->first()->value, true);
-            $client->setAccessToken($token);
-
-            if ($client->isAccessTokenExpired()) {
-                $client->refreshToken(array_search('refresh_token', $token));
-                $token = $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
-
-                Key::updateOrCreate(
-                    ['slug' => config('google-api.my-business.access-token-slug')],
-                    ['value' => json_encode($token)]
-                );
-            }
+            $client = $this->configureClient(app()->make(Google_Client::class), 'my-business');
 
             return new Google_Service_MyBusiness($client);
         });
+
+        $this->app->bind(Google_Service_YouTube::class, function () {
+            $client = $this->configureClient(app()->make(Google_Client::class), 'youtube');
+
+            return new Google_Service_YouTube($client);
+        });
+    }
+
+    protected function configureClient(Google_Client $client, string $configKey) : Google_Client {
+        if (!config()->has("google-api.$configKey")) {
+            throw new Exception('Invalid Google service configuration specified.');
+        }
+
+        $client->setAuthConfig(config("google-api.$configKey.client-secret"));
+        $client->addScope(config("google-api.$configKey.scopes"));
+        $client->setAccessType('offline');
+
+        $token = json_decode(Key::where('slug', config("google-api.$configKey.access-token-slug"))->firstOrFail()->value, true);
+        $client->setAccessToken($token);
+
+        if ($client->isAccessTokenExpired()) {
+            $client->refreshToken(array_search('refresh_token', $token));
+            $token = $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+
+            Key::updateOrCreate(
+                ['slug' => config("google-api.$configKey.access-token-slug")],
+                ['value' => json_encode($token)]
+            );
+        }
+        
+        return $client;
     }
 }
