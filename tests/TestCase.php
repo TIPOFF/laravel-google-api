d9112ac8f59069a6d5d53ec7ed801da79e26d8c8
Kyle Barney <kyle@kylebarney.dev>
2021-02-27 16:35:00 -0700

    Add test case for GMB client creation

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,27 +5,11 @@ declare(strict_types=1);
 namespace Tipoff\GoogleApi\Tests;
 
 use Tipoff\GoogleApi\GoogleApiServiceProvider;
-use Tipoff\GoogleApi\Models\Key;
 use Tipoff\Support\SupportServiceProvider;
 use Tipoff\TestSupport\BaseTestCase;
 
 class TestCase extends BaseTestCase
 {
-    public function setUp(): void
-    {
-        parent::setUp();
-
-        if (file_exists(dirname(__DIR__) . '/.env')) {
-            $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__.'/../');
-            $dotenv->load();
-
-            Key::updateOrCreate(
-                ['slug' => config('google-api.my-business.access-token-slug')],
-                ['value' => env('GOOGLE_MYBUSINESS_ACCESS_TOKEN_VALUE')]
-            );
-        }
-    }
-
     protected function getPackageProviders($app)
     {
         return [
---
+++ tests/Unit/Services/GoogleServicesTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tipoff\GoogleApi\Tests\Unit\Services;
+
+use Google_Service_MyBusiness;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tipoff\GoogleApi\Models\Key;
+use Tipoff\GoogleApi\Tests\TestCase;
+
+class GoogleServicesTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        if (file_exists(dirname(__DIR__) . '/../../.env.test')) {
+            $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__.'/../../../', '.env.test');
+            $dotenv->load();
+
+            config(['google-api' => include(realpath(__DIR__.'/../../../config/google-api.php'))]);
+
+            Key::updateOrCreate(
+                ['slug' => config('google-api.my-business.access-token-slug')],
+                ['value' => env('GOOGLE_MYBUSINESS_ACCESS_TOKEN_VALUE')]
+            );
+        }
+    }
+
+    /** @test */
+    public function it_builds_the_Google_My_Business_service() {
+        $service = app()->make(Google_Service_MyBusiness::class);
+
+        $this->assertInstanceOf(Google_Service_MyBusiness::class, $service);
+    }
+}
