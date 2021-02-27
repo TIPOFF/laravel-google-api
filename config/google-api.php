947e69bcb371f244f7c5faeac846dae0af0ab63c
Kyle Barney <kyle@kylebarney.dev>
2021-02-27 10:54:54 -0700

    Add YouTube config

--- config/google-api.php
+++ config/google-api.php
@@ -16,4 +16,20 @@ return [
         ],
         'access-token-slug' => env('GOOGLE_MYBUSINESS_ACCESS_TOKEN_SLUG', env('GOOGLE_ACCESS_TOKEN_SLUG', 'gmb-token')),
     ],
+
+    'youtube' => [
+        'client-secret' => [
+            "web" => [
+                "client_id" => env('YOUTUBE_CLIENT_ID') ?? env('GOOGLE_CLIENT_ID'),
+                "project_id" => env('YOUTUBE_PROJECT_ID') ?? env('GOOGLE_PROJECT_ID'),
+                "auth_uri" => env('YOUTUBE_AUTH_URI', env('GOOGLE_AUTH_URI', 'https://accounts.google.com/o/oauth2/auth')),
+                "token_uri" => env('YOUTUBE_TOKEN_URI', env('GOOGLE_TOKEN_URI', 'https://oauth2.googleapis.com/token')),
+                "auth_provider_x509_cert_url" => env('YOUTUBE_CERT_URL', env('GOOGLE_CERT_URL', 'https://www.googleapis.com/oauth2/v1/certs')),
+                "client_secret" => env('YOUTUBE_CLIENT_SECRET') ?? env('GOOGLE_CLIENT_SECRET'),
+                "redirect_uris" => explode('|', env('YOUTUBE_REDIRECT_URIS') ?? env('GOOGLE_REDIRECT_URIS')),
+                "javascript_origins" => explode('|', env('YOUTUBE_JAVASCRIPT_ORIGINS') ?? env('GOOGLE_JAVASCRIPT_ORIGINS')),
+            ],
+        ],
+        'access-token-slug' => env('YOUTUBE_ACCESS_TOKEN_SLUG', env('GOOGLE_ACCESS_TOKEN_SLUG', 'youtube-token')),
+    ],
 ];
