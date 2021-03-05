85f39ceff1b581469c8116a0f2d4200bdf231ae7
Kyle Barney <kyle@kylebarney.dev>
2021-03-04 20:24:05 -0700

    Add youtube-analytics config options

--- config/google-api.php
+++ config/google-api.php
@@ -35,6 +35,23 @@ return [
         'access-token-slug' => env('YOUTUBE_ACCESS_TOKEN_SLUG', env('GOOGLE_ACCESS_TOKEN_SLUG', 'youtube-token')),
     ],
 
+    'youtube-analytics' => [
+        'client-secret' => [
+            "web" => [
+                "client_id" => env('YOUTUBE_ANALYTICS_CLIENT_ID') ?? env('GOOGLE_CLIENT_ID'),
+                "project_id" => env('YOUTUBE_ANALYTICS_PROJECT_ID') ?? env('GOOGLE_PROJECT_ID'),
+                "auth_uri" => env('YOUTUBE_ANALYTICS_AUTH_URI', env('GOOGLE_AUTH_URI', 'https://accounts.google.com/o/oauth2/auth')),
+                "token_uri" => env('YOUTUBE_ANALYTICS_TOKEN_URI', env('GOOGLE_TOKEN_URI', 'https://oauth2.googleapis.com/token')),
+                "auth_provider_x509_cert_url" => env('YOUTUBE_ANALYTICS_CERT_URL', env('GOOGLE_CERT_URL', 'https://www.googleapis.com/oauth2/v1/certs')),
+                "client_secret" => env('YOUTUBE_ANALYTICS_CLIENT_SECRET') ?? env('GOOGLE_CLIENT_SECRET'),
+                "redirect_uris" => explode('|', env('YOUTUBE_ANALYTICS_REDIRECT_URIS') ?? env('GOOGLE_REDIRECT_URIS', '')),
+                "javascript_origins" => explode('|', env('YOUTUBE_ANALYTICS_JAVASCRIPT_ORIGINS') ?? env('GOOGLE_JAVASCRIPT_ORIGINS', '')),
+                "scopes" => explode('|', env('YOUTUBE_ANALYTICS_SCOPES') ?? env('GOOGLE_SCOPES', '')),
+            ],
+        ],
+        'access-token-slug' => env('YOUTUBE_ANALYTICS_ACCESS_TOKEN_SLUG', env('GOOGLE_ACCESS_TOKEN_SLUG', 'youtube-analytics-token')),
+    ],
+
     'test' => [
         'mock-json-token' => '{"access_token":"mock-access-token","expires_in":3599,"scope":"https:\/\/www.googleapis.com\/auth\/business.manage","token_type":"Bearer","created":'.time().',"refresh_token":"mock-refresh-token"}',
     ]
