fff32928059cb69ab281eccd32debb2f8b4dfe09
Kyle Barney <kyle@kylebarney.dev>
2021-03-04 19:51:39 -0700

    Add test for YouTube service instantiation

--- tests/Unit/Services/GoogleServicesTest.php
+++ tests/Unit/Services/GoogleServicesTest.php
@@ -3,6 +3,7 @@
 namespace Tipoff\GoogleApi\Tests\Unit\Services;
 
 use Google_Service_MyBusiness;
+use Google_Service_YouTube;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tipoff\GoogleApi\Models\Key;
 use Tipoff\GoogleApi\Tests\TestCase;
@@ -15,6 +16,9 @@ class GoogleServicesTest extends TestCase
     {
         parent::setUp();
 
+        // Because of the order in which Testbench loads things, we don't
+        // have access to our .env.test variables when the config is initially
+        // set. Override the config settings here so that we can test properly.
         if (file_exists(dirname(__DIR__) . '/../../.env.test')) {
             $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__.'/../../../', '.env.test');
             $dotenv->load();
@@ -41,5 +45,20 @@ class GoogleServicesTest extends TestCase
         $service = app()->make(Google_Service_MyBusiness::class);
 
         $this->assertInstanceOf(Google_Service_MyBusiness::class, $service);
+    }
+
+    /** @test */
+    public function it_builds_the_Google_YouTube_service() {
+        // If we don't have an actual key for our testing in the .env.test
+        // file, create a mock one here that the Google client will accept
+        // for creating the client.
+        Key::firstOrCreate(
+            ['slug' => config('google-api.youtube.access-token-slug')],
+            ['value' => config('google-api.test.mock-json-token')]
+        );
+
+        $service = app()->make(Google_Service_YouTube::class);
+
+        $this->assertInstanceOf(Google_Service_YouTube::class, $service);
     }
 }
